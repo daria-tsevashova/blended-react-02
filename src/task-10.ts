48208c5b765d60371c8cccd9886e05a35bbabd22
Dari <dariatsevashova@gmail.com>
2025-11-06 19:24:47 +0100

    task-10

--- src/task-10.ts
+++ src/task-10.ts
@@ -3,7 +3,6 @@ const users = ["alice", "bob", "charlie"];
 
 // Завдання:
 // 1. Створіть типізовану функцію toUserObjects, яка приймає масив рядків (імен користувачів).
-
 // 2. Усередині функції переберіть масив імен та для кожного імені створи об’єкт з такими властивостями:
 // id — порядковий номер (починаючи з 1),
 // name — саме ім’я користувача(рядок з масиву).
@@ -11,5 +10,17 @@ const users = ["alice", "bob", "charlie"];
 // 3. Функція повинна повертати масив отриманих об’єктів.
 
 // 4. Переконайтеся, що результат функції має правильну типізацію, а TypeScript не видає помилок.
+
+interface User {
+  name: string;
+  id: number;
+}
+
+function toUserObjects(names: string[]): User[] {
+  return names.map((name, index) => ({
+    id: index + 1,
+    name,
+  }));
+}
 
 toUserObjects(users); // [{ id: 1, name: "alice" }, { id: 2, name: "bob" }, { id: 3, name: "charlie" }]
